0b1d16dfa0bde33d82a02429124a3309c79c6c3e
Ashok Thapa <mgr.axok@gmail.com>
2026-07-11 01:46:27 +0545

    Update docker_push.groovy

--- vars/docker_push.groovy
+++ vars/docker_push.groovy
@@ -1,10 +1,19 @@
+// def call(String Project, String ImageTag, String dockerhubUser){
+//     withCredentials([usernamePassword(credentialsId: 'docker',
+//                                      passwordVariable: 'DOCKER_PASS',
+//                                      usernameVariable: 'DOCKER_USER')]) {
+//         sh """
+//             docker login -u ${DOCKER_USER} -p ${DOCKER_PASS}
+//             docker push ${dockerhubUser}/${Project}:${ImageTag}
+//         """
+//     }
+// }
+
 def call(String Project, String ImageTag, String dockerhubUser){
     withCredentials([usernamePassword(credentialsId: 'docker',
                                      passwordVariable: 'DOCKER_PASS',
                                      usernameVariable: 'DOCKER_USER')]) {
-        sh """
-            docker login -u ${DOCKER_USER} -p ${DOCKER_PASS}
-            docker push ${dockerhubUser}/${Project}:${ImageTag}
-        """
+        sh 'echo $DOCKER_PASS | docker login -u $DOCKER_USER --password-stdin'
     }
+    sh "docker push ${dockerhubUser}/${Project}:${ImageTag}"
 }
