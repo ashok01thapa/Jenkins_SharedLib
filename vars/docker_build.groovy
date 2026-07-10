2914338a701be7192301cf5c45b5ca8cf74af796
Ashok Thapa <mgr.axok@gmail.com>
2026-07-11 01:05:15 +0545

    Update docker_build.groovy

--- vars/docker_build.groovy
+++ vars/docker_build.groovy
@@ -4,13 +4,6 @@
 // }
 
 
-def call(String imageName, String tag, String dockerHubUser, String nextPublicHost, String mongoCredentialsId) {
-    withCredentials([string(credentialsId: "${mongoCredentialsId}", variable: 'MONGODB_URI')]) {
-        sh """
-            docker build \
-              --build-arg NEXT_PUBLIC_HOST=${nextPublicHost} \
-              --build-arg MONGODB_URI=\$MONGODB_URI \
-              -t ${dockerHubUser}/${imageName}:${tag} .
-        """
-    }
+def call(String ProjectName, String ImageTag, String DockerHubUser){
+    sh "docker push ${DockerHubUser}/${ProjectName}:${ImageTag}"
 }
