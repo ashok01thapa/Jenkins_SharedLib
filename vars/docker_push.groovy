0309712a04f29c6098d5208fd19049dc7c3f1480
Ashok Thapa <mgr.axok@gmail.com>
2026-07-12 19:03:46 +0545

    Update docker_push.groovy

--- vars/docker_push.groovy
+++ vars/docker_push.groovy
@@ -1,19 +1,21 @@
+// def call(String Project, String ImageTag, String dockerhubUser){
+//     withCredentials([usernamePassword(credentialsId: 'docker',
+//                                      passwordVariable: 'DOCKER_PASS',
+//                                      usernameVariable: 'DOCKER_USER')]) {
+//         sh """
+//             docker login -u ${DOCKER_USER} -p ${DOCKER_PASS}
+//             docker push ${dockerhubUser}/${Project}:${ImageTag}
+//         """
+//     }
+// }
+
 def call(String Project, String ImageTag, String dockerhubUser){
     withCredentials([usernamePassword(credentialsId: 'docker',
                                      passwordVariable: 'DOCKER_PASS',
                                      usernameVariable: 'DOCKER_USER')]) {
         sh """
-            docker login -u ${DOCKER_USER} -p ${DOCKER_PASS}
+            echo "\$DOCKER_PASS" | docker login -u "\$DOCKER_USER" --password-stdin
             docker push ${dockerhubUser}/${Project}:${ImageTag}
         """
     }
 }
-
-// def call(String Project, String ImageTag, String dockerhubUser){
-//     withCredentials([usernamePassword(credentialsId: 'docker',
-//                                      passwordVariable: 'DOCKER_PASS',
-//                                      usernameVariable: 'DOCKER_USER')]) {
-//         sh 'echo $DOCKER_PASS | docker login -u $DOCKER_USER --password-stdin'
-//     }
-//     sh "docker push ${dockerhubUser}/${Project}:${ImageTag}"
-// }
