3c8d54c80e2338e08ee6071553f8b255785fa515
Ashok Thapa <mgr.axok@gmail.com>
2026-07-12 20:13:06 +0545

    Update owasp_dependency.groovy

--- vars/owasp_dependency.groovy
+++ vars/owasp_dependency.groovy
@@ -1,6 +1,7 @@
+// vars/owasp_dependency_check.groovy
 def call() {
     withCredentials([string(credentialsId: 'nvd-api-key', variable: 'NVD_API_KEY')]) {
-        dependencyCheck additionalArguments: '--scan . --format XML --project bitlinks --nvdApiKey ' + NVD_API_KEY + ' --nvdApiDelay 6000',
+        dependencyCheck additionalArguments: "--scan . --format XML --project bitlinks --nvdApiKey ${NVD_API_KEY} --nvdApiDelay 6000",
                          odcInstallation: 'OWASP'
     }
     dependencyCheckPublisher pattern: 'dependency-check-report.xml'
